77a786995f9305895a774fa9441e51e2d2f327bd
Kieran132 <kiemclean@hotmail.co.uk>
2022-12-28 17:15:50 +0000

    Add function for players to choose symbol

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ import random
 
 def main():
     intro_message = intro()
-    board = createBoard()
+    board = create_board()
     print_board = printBoard()
-    player_symbol = playerSym()
+    player_symbol = player_sym()
     checker = boardCheck(board, player_1, player_2)
 
 
@@ -16,8 +16,26 @@ def intro():
     print("First to get 3 in a row wins!/n")
     print("Press enter to continue /n")
 
-def createBoard():
+def create_board():
+    """
+    Function to create layout of a blank board
+    """
     board = [[" ", " ", " "],
              [" ", " ", " "],
              [" ", " ", " "]]        
     return board
+
+def player_sym():
+    """
+    Function that allows the players to choose which symbol they want
+    """
+    player_1 = input("Player 1, please choose your symbol, X or O?")
+    if player_1 == "X":
+        player_2 = "O"
+        print("Player 2, you are O")
+    else:
+        player_2 = "X"
+        print("Player 2, you are X")
+    input("Press Enter to play!")
+    print("/n")
+    return (player_1, player_2)
