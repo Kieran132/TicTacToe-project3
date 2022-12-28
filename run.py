9dd1fbb3423f1271297706992df8f0703ff8131f
Kieran132 <kiemclean@hotmail.co.uk>
2022-12-28 18:07:34 +0000

    Add docstring to main function

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 def main():
+    """
+    Functions needed to run the game
+    """
     intro()
     board = create_board()
     printing_board(board)
@@ -10,9 +13,9 @@ def intro():
     """
     Function to give a message for the player and outline rules
     """
-    print("Ready to play Tic Tac Toe?!/n")
-    print("First to get 3 in a row wins!/n")
-    print("Press enter to continue /n")
+    print("Ready to play Tic Tac Toe?!")
+    print("First to get 3 in a row wins!")
+    print("Press enter to continue")
 
 
 def create_board():
@@ -21,7 +24,7 @@ def create_board():
     """
     board = [[" ", " ", " "],
              [" ", " ", " "],
-             [" ", " ", " "]]        
+             [" ", " ", " "]]
     return board
 
 
@@ -54,21 +57,18 @@ def start(board, player_1, player_2, count):
                     "[upper row: enter 0, middle row: enter 1, bottom row: enter 2]:"))
     column = int(input("Pick a column:"
                        "[left column: enter 0, middle column: enter 1, right column enter 2]"))
-
     while (row > 2 or row < 0) or (column > 2 or column < 0):
         off_board(row, column)
         row = int(input("Pick a row[upper row:"
                         "[enter 0, middle row: enter 1, bottom row: enter 2]:"))
         column = int(input("Pick a column:"
                            "[left column: enter 0, middle column: enter 1, right column enter 2]"))
-
     while (board[row][column] == player_1) or (board[row][column] == player_2):
         wrong(board, player_1, player_2, row, column)
         row = int(input("Pick a row[upper row:"
                         "[enter 0, middle row: enter 1, bottom row: enter 2]:"))
         column = int(input("Pick a column:"
-                            "[left column: enter 0, middle column: enter 1, right column enter 2]")) 
-        
+                            "[left column: enter 0, middle column: enter 1, right column enter 2]"))     
     if player == player_1:
         board[row][column] = player_1
     else:
