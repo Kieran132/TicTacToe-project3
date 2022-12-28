4eb254ae04fb6f830632259c34c191d083960578
Kieran132 <kiemclean@hotmail.co.uk>
2022-12-28 17:08:29 +0000

    Set out intro message and created function to show blank game are

--- run.py
+++ run.py
@@ -6,3 +6,18 @@ def main():
     print_board = printBoard()
     player_symbol = playerSym()
     checker = boardCheck(board, player_1, player_2)
+
+
+def intro():
+    """
+    Function to give a message for the player and outline rules
+    """
+    print("Ready to play Tic Tac Toe?!/n")
+    print("First to get 3 in a row wins!/n")
+    print("Press enter to continue /n")
+
+def createBoard():
+    board = [[" ", " ", " "],
+             [" ", " ", " "],
+             [" ", " ", " "]]        
+    return board
